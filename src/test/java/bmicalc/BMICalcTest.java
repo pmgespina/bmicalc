cddaed412331174893bde3fd35b04bdc2bc01bf6
pmgespina <pmgespina@uma.es>
2023-02-21 16:58:21 +0100

    tests of method category

--- src/test/java/bmicalc/BMICalcTest.java
+++ src/test/java/bmicalc/BMICalcTest.java
@@ -58,4 +58,76 @@ public class BMICalcTest {
 		assertEquals(20.73, calc.bmi(75, 1.90));
 	}
 	
+	@Test
+	@DisplayName("Category with negative bmi")
+	public void category1() {
+		assertThrows(RuntimeException.class, () -> calc.category(-2));
+	}
+	
+	@Test
+	@DisplayName("Category with bmi = 0")
+	public void category2() {
+		assertThrows(RuntimeException.class, () -> calc.category(0));
+	}
+	
+	@Test
+	@DisplayName("Category with bmi = 10")
+	public void category3() {
+		assertEquals("UNDERWEIGHT", calc.category(10));
+	}
+	
+	@Test
+	@DisplayName("Category with bmi = 18.4")
+	public void category4() {
+		assertEquals("UNDERWEIGHT", calc.category(18.4));
+	}
+	
+	@Test
+	@DisplayName("Category with bmi = 18.5")
+	public void category5() {
+		assertEquals("NORMAL", calc.category(18.5));
+	}
+
+	@Test
+	@DisplayName("Category with bmi = 20")
+	public void category6() {
+		assertEquals("NORMAL", calc.category(20));
+	}
+	
+	@Test
+	@DisplayName("Category with bmi = 24.9")
+	public void category7() {
+		assertEquals("NORMAL", calc.category(24.9));
+	}
+	
+	@Test
+	@DisplayName("Category with bmi = 25")
+	public void category8() {
+		assertEquals("OVERWEIGHT", calc.category(25));
+	}
+	
+	@Test
+	@DisplayName("Category with bmi = 27")
+	public void category9() {
+		assertEquals("OVERWEIGHT", calc.category(27));
+	}
+	
+	@Test
+	@DisplayName("Category with bmi = 29.9")
+	public void category10() {
+		assertEquals("OVERWEIGHT", calc.category(29.9));
+	}
+	
+	@Test
+	@DisplayName("Category with bmi = 30")
+	public void category11() {
+		assertEquals("OBESE", calc.category(30));
+	}
+	
+	@Test
+	@DisplayName("Category with bmi = 35")
+	public void category12() {
+		assertEquals("OBESE", calc.category(35));
+	}
+	
 }
