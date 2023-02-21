dd25815ad4699c0c70509bdb049a20f9b6b4854d
pmgespina <pmgespina@uma.es>
2023-02-21 17:48:45 +0100

    implementation of calculator methods

--- src/main/java/bmicalc/BMICalcImpl.java
+++ src/main/java/bmicalc/BMICalcImpl.java
@@ -3,15 +3,39 @@ package bmicalc;
 public class BMICalcImpl implements BMICalc {
 
 	public double bmi(double mass, double height) {
-		return 0.0;
+		if (mass <= 0 || height <= 0) {
+			throw new RuntimeException("EXCEPTION: Invalid arguments");
+		} else {
+			return mass / Math.pow(height, 2);
+		}
 	}
 
 	public String category(double bmi) {
-		return "";
+		String res = "";
+		if (bmi < 18.5 && bmi > 0) {
+			res = "UNDERWEIGHT";
+		} else if (bmi >= 18.5 && bmi <= 24.9) {
+			res = "NORMAL";
+		} else if (bmi >= 25 && bmi <= 29.9) {
+			res = "OVERWEIGHT";
+		} else if (bmi >= 30) {
+			res = "OBESE";
+		} else {
+			throw new RuntimeException("EXCEPTION: Invalid arguments");
+		}
+		return res;
 	}
 
 	public boolean abdominalObesity(double waistCircumference, char gender) {
-		return false;
+		boolean res = false;
+		if (waistCircumference <= 0 || (gender != 'M' && gender != 'F')) {
+			throw new RuntimeException("EXCEPTION: Invalid arguments");
+		} else if (gender == 'F' && waistCircumference > 80) {
+			res = true;
+		} else if (gender == 'M' && waistCircumference > 90) {
+			res = true;
+		}
+		return res;
 	}
 
 }
