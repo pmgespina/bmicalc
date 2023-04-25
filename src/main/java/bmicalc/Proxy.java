e62fa89c78ae86f229f1b4630723f06fb3283eac
pmgespina <pmgespina@uma.es>
2023-04-25 13:53:36 +0200

    proxy class added

---
+++ src/main/java/bmicalc/Proxy.java
@@ -0,0 +1,53 @@
+package bmicalc;
+
+import java.util.Iterator;
+import java.util.Map;
+
+public class Proxy implements IMCStats, IMCHospital {
+
+	private Adapter calculator;
+	private static double alturas;
+	private static double pesos;
+	private static double imcs;
+	private static int cnt;
+
+	@Override
+	public Map<Double, String> imc(double altura, double peso) {
+		alturas += altura;
+		pesos += peso;
+		Map<Double, String> res = calculator.imc(altura, peso);
+		Iterator<Double> it = res.keySet().iterator();
+		if(it.hasNext()) {
+			Double key = it.next();
+			imcs += key;
+		}
+		cnt++;
+		return res;
+	}
+
+	@Override
+	public boolean tieneObesidadAbdominal(char genero, double circunferencia) {
+		return calculator.tieneObesidadAbdominal(genero, circunferencia);
+	}
+
+	@Override
+	public double alturaMedia() {
+		return alturas / cnt;
+	}
+
+	@Override
+	public double pesoMedio() {
+		return pesos / cnt;
+	}
+
+	@Override
+	public double imcMedio() {
+		return imcs / cnt;
+	}
+
+	@Override
+	public int numPacientes() {
+		return cnt;
+	}
+
+}
